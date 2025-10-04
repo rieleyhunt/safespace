5d7694e6598854b73f3e54335aba1387a33f33c7
Rieley <huntrieley@gmail.com>
2025-10-04 14:22:44 -0400

    Co-authored-by: Richnicky <richnicky123@gmail.com> Co-authored-by: keenas7 <keenas7@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-console.log("Clicking and doing things");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('__dirname'));
+
+app.get('/button-clicked', (req, res) => {
+    res.json({message: 'button was clicked!'})
+});
+ 
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
